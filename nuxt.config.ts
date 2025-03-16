451de6f8abe82fb41703b6b64e0c7c37f65005c8
Tobias Blum <tobiasblum02@gmail.com>
2025-03-16 11:25:05 +0100

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/eslint'],
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2025-03-16',
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+})
